76cc92478a017686ac4a5a50e54d1073d5291451
LQW <1974316188@qq.com>
2022-07-04 15:00:07 +0800

    feat(components): 面包屑

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="navbar clearfix">
-    <div class="navbar-left">left</div>
+    <div class="navbar-left">
+      <Hamburger class="hamburger-container"></Hamburger>
+      <Breadcrumb></Breadcrumb>
+    </div>
     <div class="navbar-right">
       <el-dropdown trigger="click" @command="handleCommand">
         <span class="el-dropdown-link">
-          <el-avatar shape="square" :size="40" :src="avatarUrl" />
+           <el-avatar shape="square" :size="40" :src="avatarUrl" />
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item command="home">首页</el-dropdown-item>
-            <el-dropdown-item command="profile">课程主页</el-dropdown-item>
-            <el-dropdown-item command="logout" divided
-              >退出登录</el-dropdown-item
-            >
+            <el-dropdown-item command="home" >首页</el-dropdown-item>
+            <el-dropdown-item command="profile" >课程主页</el-dropdown-item>
+            <el-dropdown-item command="logout" divided >退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
@@ -24,6 +25,8 @@
 import { computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import Breadcrumb from '../components/Breadcrumb'
+import Hamburger from '../components/Hamburger'
 const store = useStore()
 const router = useRouter()
 
@@ -36,7 +39,7 @@ const handleCommand = (command) => {
     case 'home':
       handleToHome()
       break
-    case 'logout':
+    case 'logout' :
       handleLogout()
       break
   }
@@ -57,28 +60,32 @@ const handleLogout = async () => {
 </script>
 
 <style lang="scss" scoped>
-.navbar {
+.navbar{
   height: 50px;
   line-height: 50px;
   overflow: hidden;
   position: relative;
   background: #fff;
   box-shadow: 0 1px 4px rgb(0 21 41 / 8%);
-  padding: 0 20px;
+  padding: 0 20px 0 0;
 
-  .navbar-left {
-    float: left;
+  .navbar-left{
+    float :left;
   }
-  .navbar-right {
-    float: right;
-  }
-
-  .el-dropdown {
-    margin: 5px 10px 0 0;
+  .navbar-right{
+    float :right;
   }
 
-  .el-avatar {
-    background: chocolate;
+  .el-dropdown{
+    margin : 5px 10px 0 0;
+  }
+
+  .el-avatar{
+    background : goldenrod;
+  }
+
+  .hamburger-container{
+    float : left;
   }
 }
 </style>
